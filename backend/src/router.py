58f9cb6bc6975f4aefed2d0eb702ebc5a2496fdc
ynori123 <norisama1159@gmail.com>
2023-11-22 14:31:09 +0900

    add routes(get all castles)

--- backend/src/router.py
+++ backend/src/router.py
@@ -1,4 +1,19 @@
-from fastapi import APIRouter
+from fastapi import (
+    APIRouter,
+    Depends
+)
+from typing import List
+from src.schema import (
+    ResponseCastle
+)
+from src.model import (
+    Castle
+)
+from src.crud import (
+    get_castles
+)
+from src.database import get_db
+from sqlalchemy.orm import Session
 
 router = APIRouter()
 
@@ -6,3 +21,7 @@ router = APIRouter()
 @router.get("/ping")
 def ping():
     return {"ping" : "pong"}
+
+@router.get("/castles", response_model=List[ResponseCastle])
+def get_castles() -> List[Castle]:
+    return get_castles()
