953ba3c42a78d0c0df83a674056608efbe4e6b4c
ynori123 <norisama1159@gmail.com>
2023-11-27 10:54:56 +0900

    created by alembic

--- backend/src/crud.py
+++ backend/src/crud.py
@@ -8,3 +8,7 @@ from src.database import get_db
 def get_castles(db: Session = Depends(get_db)) -> List[Castle]:
     castles = db.query(Castle).all()
     return castles
+
+# def get_castle(id: int, db: Session = Depends(get_db)) -> Castle[Castle]:
+#     castle = db.query(Castle).filter_by(id=id).first()
+#     return castle
