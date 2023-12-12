b430213ef14674e91271c4b9c775f9542bd6b600
ynori123 <norisama1159@gmail.com>
2023-12-12 11:52:29 +0900

    CORSについて追記

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
 from router import router as rt
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(router=rt)
