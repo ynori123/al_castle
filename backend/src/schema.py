5baa88fa1d3e462650b8cc081eb3970bfefe7c7c
ynori123 <norisama1159@gmail.com>
2023-12-19 12:09:09 +0900

    Travel スキーマの実装

--- backend/src/schema.py
+++ backend/src/schema.py
@@ -16,5 +16,22 @@ class ResponseCastle(BaseModel):
 
 class RequestCastle(BaseModel):
     castle_id: int
+    
+    class Congig:
+        orm_mode = True
+class ResponseTravel(BaseModel):
+    castle1: str
+    castle2: str
+    admission_distance: float
+    way_distance: float
+    time: str
 
+    class Congig:
+        orm_mode = True
 
+class RequestTravel(BaseModel):
+    castle1: int
+    castle2: int
+
+    class Congig:
+        orm_mode = True
