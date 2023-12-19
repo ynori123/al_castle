669a9cb0488e39f7f5c85d6fc033cbb74a017442
Seaeees <seaeees@gmail.com>
2023-12-19 12:00:10 +0900

    コード整形

--- frontend/src/app/components/Card.tsx
+++ frontend/src/app/components/Card.tsx
@@ -1,23 +1,29 @@
 type Props = {
-	id: number;
+  id: number;
   title: string;
-	discription: string;
-	tags?: string[];
-	img: string;
+  discription: string;
+  tags?: string[];
+  img: string;
 };
 
-const Card = ({id, title, discription, img, tags}: Props) => {
+const Card = ({ id, title, discription, img, tags }: Props) => {
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg m-4">
-			<img className="w-full aspect-video overflow-hidden object-cover" src={img} alt={title}/>
-			<div className="px-6 py-4">
-				<div className="font-bold text-xl mb-2">{id}. {title}</div>
-				<p className="text-gray-700 text-base overflow-ellipsis whitespace-nowrap overflow-hidden">
-					{discription}
-				</p>
-			</div>
-		</div>
-	);
-}
+      <img
+        className="w-full aspect-video overflow-hidden object-cover"
+        src={img}
+        alt={title}
+      />
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">
+          {id}. {title}
+        </div>
+        <p className="text-gray-700 text-base overflow-ellipsis whitespace-nowrap overflow-hidden">
+          {discription}
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
--- frontend/src/app/components/Header.tsx
+++ frontend/src/app/components/Header.tsx
@@ -1,18 +1,16 @@
 type Props = {
   title: string;
-	discription?: string;
+  discription?: string;
 };
 
-const Header = ({title, discription}: Props) => {
+const Header = ({ title, discription }: Props) => {
   return (
-		<header className="header pl-6">
-			<h1 className="text-3xl font-bold pb-4 pt-8">{title}</h1>
+    <header className="header pl-6">
+      <h1 className="text-3xl font-bold pb-4 pt-8">{title}</h1>
 
-			{discription && (
-				<p className="text-sm text-slate-700">{discription}</p>
-			)}
-		</header>
-	);
-}
+      {discription && <p className="text-sm text-slate-700">{discription}</p>}
+    </header>
+  );
+};
 
-export default Header
+export default Header;
