388ca93ff4b86ab02654d3f496fef1b0da218088
ynori123 <norisama1159@gmail.com>
2023-12-19 11:58:54 +0900

    DBのURIを環境変数から読み込むように変更

--- backend/src/database.py
+++ backend/src/database.py
@@ -1,12 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from src.config import Settiings
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///database.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    Settiings().DATABASE_URL, connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
