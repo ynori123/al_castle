7d15a14608c55e855d55ba7fb9b3f33379416977
Seaeees <seaeees@gmail.com>
2023-12-26 10:45:30 +0900

    Navbar周辺のデザイン変更

--- frontend/src/app/castles/page.tsx
+++ frontend/src/app/castles/page.tsx
@@ -1,9 +1,11 @@
 import Card from "../components/Card";
 import Header from "../components/Header";
+import Navbar from "../components/Navbar";
 
 export default function Castles() {
   return (
     <>
+      <Navbar />
       <main className="container mx-auto">
         <Header
           title="城一覧"
