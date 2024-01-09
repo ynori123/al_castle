a86a3d65c017e6ce479816f05da7b5ff4fed8397
zakuro <Yuu1ka102020@gmail.com>
2024-01-09 11:39:53 +0900

    testメソッドの実装及び旧testメソッドの削除

--- backend/src/idokeido.py
+++ backend/src/idokeido.py
@@ -21,14 +21,3 @@ def calc_distance(lat1, lon1, lat2, lon2):
     distance = 6371 * c  # 地球の半径を6371kmとして距離を計算
 
     return distance
-
-def test_calc_distance() -> None:
-    # テスト用の緯度・経度
-    lat1 = 35.6895
-    lon1 = 139.6917
-    lat2 = 34.7025
-    lon2 = 135.4958
-
-    # 二地点間の距離を計算
-    distance = calc_distance(lat1, lon1, lat2, lon2)
-    print("地点A ",lat1,",",lon1,"\n地点B ",lat2,",",lon2,"\n距離:",distance,"\n")
