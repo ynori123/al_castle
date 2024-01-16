4ec06213f55985b047f2dc76257ad8f7d8faec56
ynori123 <norisama1159@gmail.com>
2024-01-16 11:29:50 +0900

    current route検索

--- backend/src/crud.py
+++ backend/src/crud.py
@@ -1,7 +1,7 @@
 from typing import List
 from sqlalchemy.orm import Session
 from sqlalchemy.engine import Result
-from src.model import Castle
+from src.model import Castle, CastleDistance
 from fastapi import Depends
 from src.database import get_db
 
@@ -13,3 +13,6 @@ def fetch_specific_castles(db: Session, id: int) -> Castle:
     castle = db.query(Castle).filter_by(id=id).first()
     return castle
 
+def fetch_current_route(db: Session, dep_id: int, arr_id: int) -> Result:
+    current_route = db.query(CastleDistance).filter_by(castle_id_1=dep_id, arr_id=arr_id).first()
+    return current_route
