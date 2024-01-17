6cc1eeb203815382535f1cb4c53e918fc7c4341d
ynori123 <norisama1159@gmail.com>
2024-01-18 02:55:07 +0900

    sampleページの作成

---
+++ frontend/src/app/sample/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+export default function Page() {
+  return (
+    <div>
+      <Navbar />
+      <main className="container mx-auto">
+        
+      </main>
+    </div>
+  )
+}
