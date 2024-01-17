3eb50e8cace6f572c5c407e02021bfbfb2e3470c
ynori123 <norisama1159@gmail.com>
2024-01-17 18:33:03 +0900

    不要なタグの削除

--- frontend/src/app/travel/page.tsx
+++ frontend/src/app/travel/page.tsx
@@ -9,6 +9,8 @@ import { useEffect, useState } from "react";
 //
 export default function Travel() {
   const [castleData, setCastleData] = useState<{ id: number, name: string, prefecture: string }[]>([]);
+  const [start, setStart] = useState<string>("");
+  const [goal, setGoal] = useState<string>("");
   const [isStop, setIsStop] = useState<boolean[]>(Array(100).fill(false));
   useEffect(() => {
     const fetchData = async () => {
@@ -31,7 +33,7 @@ export default function Travel() {
           discription="百名城の最短経路算出ツールです。行きたい城にすべてチェックを入れ，検索ボタンを押してください．"
         />
 
-        <form action="/travel/result" method="post" className="mx-64">
+        <form className="mx-64">
           {/* 全城分のチェックボックス */}
           <div className="flex mb-5">
             <label className="block text-lg font-bold mb-3 w-40">出発地</label>
@@ -39,6 +41,8 @@ export default function Travel() {
               className="bg-gray-200 appearance-none border-2 flex-1 border-gray-200 w-4/5 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
               id="inline-full-name"
               type="text"
+              value={start}
+              onChange={(e) => setStart(e.target.value)}
             />
           </div>
           <div className="flex mb-5">
@@ -47,6 +51,8 @@ export default function Travel() {
               className="bg-gray-200 appearance-none border-2 flex-1 border-gray-200 w-4/5 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
               id="inline-full-name"
               type="text"
+              value={goal}
+              onChange={(e) => setGoal(e.target.value)}
             />
             <button
               className="rounded-md bg-indigo-600 ml-6 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
@@ -64,20 +70,18 @@ export default function Travel() {
               <ul>
                 {castleData.map((item, i) => {
                   return (
-                    <>
-                      <li className="inline-block w-48">
-                        <input
-                          className="mr-2 leading-tight"
-                          type="checkbox"
-                          name="castles"
-                          checked={isStop[i]}
-                          onChange={() => setIsStop([...isStop.slice(0, i), !isStop[i], ...isStop.slice(i + 1)])}
-                        />
-                        <a className="text-sm hover:underline" href={"/castles/" + (i + 1)}>
-                          {i + 1}. {item.name}
-                        </a>
-                      </li>
-                    </>
+                    <li className="inline-block w-48">
+                      <input
+                        className="mr-2 leading-tight"
+                        type="checkbox"
+                        name="castles"
+                        checked={isStop[i]}
+                        onChange={() => setIsStop([...isStop.slice(0, i), !isStop[i], ...isStop.slice(i + 1)])}
+                      />
+                      <a className="text-sm hover:underline" href={"/castles/" + (i + 1)}>
+                        {i + 1}. {item.name}
+                      </a>
+                    </li>
                   );
                 })}
               </ul>
