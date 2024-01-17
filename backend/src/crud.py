cfac0d3e07318a302565640a9a41942706d06a4a
Se4weed <121135238+ynori123@users.noreply.github.com>
2024-01-17 15:55:16 +0900

    Update crud.py

--- backend/src/crud.py
+++ backend/src/crud.py
@@ -4,6 +4,7 @@ from sqlalchemy.engine import Result
 from src.model import Castle, CastleDistance
 from src.google_api import fetch_googlemap_api
 import re
+from fastapi import HTTPException
 
 def fetch_castles(db: Session) -> List[Castle]:
     castles = db.query(Castle).all()
@@ -18,24 +19,30 @@ def fetch_travel(db: Session, arr: str, dep: str, castles: List[int]) -> Result:
     distace = []
     way_time = []
     castle_name = []
+    way_time_str = []
     for castle in castles:
-        castle_name.append(db.query(Castle).filter_by(id=castle).first().name)
+        specific_castle_name = db.query(Castle).filter_by(id=castle).first().name
+        if specific_castle_name is None:
+            raise HTTPException(status_code=400, detail="Castle not found.")
+        castle_name.append(specific_castle_name)
     length = len(castles)
     if length >= 2:
         for i in range(1, length):
-            route[i] = db.query(CastleDistance).filter_by(dep=castles[i-1], arr=castles[i]).first()
-            if route[i] == None:
-                route[i] = fetch_googlemap_api(origin=castle_name[i-1], dest=castles[i])
-                db.add(route[i])
+            route.append(db.query(CastleDistance).filter_by(castle_id_1=castles[i-1], castle_id_2=castles[i]).first())
+            if route[i-1] == None:
+                route[i-1] = fetch_googlemap_api(origin=castle_name[i-1], dest=castle_name[i])
+                db.add(route[i-1])
                 db.commit()
-            distace[i] = route[i].distance
-            way_time[i] = route[i].time
-            
-    first_route = fetch_googlemap_api(origin=dep, dest=castles[0])
-    last_route = fetch_googlemap_api(origin=castles[length-1], dest=arr)
+            distace.append(route[i-1].distance)
+            way_time.append(route[i-1].time)
+            way_time_str.append(sec_to_str(route[i-1].time))
+    first_route = fetch_googlemap_api(origin=dep, dest=castle_name[0])
+    last_route = fetch_googlemap_api(origin=castle_name[length-1], dest=arr)
     
     distace.insert(0, first_route.distance)
     distace.append(last_route.distance)
+    way_time_str.insert(0, sec_to_str(first_route.time))
+    way_time_str.append(sec_to_str(last_route.time))
     way_time.insert(0, first_route.time)
     way_time.append(last_route.time)
     
@@ -43,26 +50,14 @@ def fetch_travel(db: Session, arr: str, dep: str, castles: List[int]) -> Result:
         "dep": dep,
         "arr": arr,
         "way_distance": distace,
-        "way_time": way_time,
-        "total_distance": sum_time(distace),
-        "total_time": sum_time(way_time)
+        "way_time": way_time_str,
+        "total_distance": sum(distace),
+        "total_time": sec_to_str(sum(way_time))
     }
 
-def sum_time(s: [str]) -> tuple(int, int):
-    h = 0
-    m = 0
-    for item in s:
-        hours, minutes = str_to_time(item)
-        h += hours
-        m += minutes
-    h += m // 60
-    m = m % 60
-    return f"{h}時間{m}分"
-
-def str_to_time(s: str) -> tuple(int, int):
-    pattern = r"(\d+)時間(\d+)分"
-    match = re.search(pattern, s)
-    if match:
-        hours = int(match.group(1))
-        minutes = int(match.group(2))
-    return (hours, minutes)
+def sec_to_str(sec: int) -> str:
+    day = sec // 86400
+    hour = (sec % 86400) // 3600
+    minute = (sec % 3600) // 60
+    if day > 0: return f"{day}日{hour}時間{minute}分"
+    else: return f"{hour}時間{minute}分"
