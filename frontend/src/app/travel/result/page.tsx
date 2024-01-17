713e68e297440cfe279d1582c06e68b0fc036503
ynori123 <norisama1159@gmail.com>
2024-01-17 21:24:02 +0900

    検索機能の実装

--- frontend/src/app/travel/result/page.tsx
+++ frontend/src/app/travel/result/page.tsx
@@ -1,11 +1,53 @@
-import Card from "../../components/Card";
-import Header from "../../components/Header";
-import Navbar from "../../components/Navbar";
-import Footer from "../../components/Footer";
+"use client";
+import Card from "@/app/components/Card";
+import Header from "@/app/components/Header";
+import Navbar from "@/app/components/Navbar";
+import Footer from "@/app/components/Footer";
+import { useEffect, useState } from "react";
 
+type Props = {
+  params: {}
+  searchParams: QueryProps
+}
+type QueryProps = {
+  dep: string,
+  arr: string,
+  castle: string
+}
+type DataResponse = {
+  dep: string,
+  arr: string,
+  castles: number[],
+  way_distance: number[],
+  way_time: string[],
+  total_distance: number,
+  total_time: string
+}
 //出発地点
 //
-export default function Result() {
+export default function Result({params, searchParams}: Props) {
+  const [result, setResult] = useState<DataResponse>({dep: searchParams.dep, arr: searchParams.arr, castles: searchParams.castle.split(',').map((item)=>Number(item)), way_distance: [0], way_time: ["計算中..."], total_distance: 0, total_time: "計算中..."});
+  useEffect(() => {
+    const search = async() => {
+      const q = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          dep: searchParams.dep,
+          arr: searchParams.arr,
+          castle: searchParams.castle.split(",")
+        })
+      }
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/travel`, q);
+      const data: DataResponse = await res.json();
+      console.log(data);
+      setResult(data);
+    }
+    search();
+  }, []); 
+
   return (
     <>
       <Navbar />
@@ -15,7 +57,12 @@ export default function Result() {
           discription="百名城の最短経路算出ツールの検索結果です．"
         />
 
-        <div className="flex flex-wrap justify-center">aa</div>
+        <div className="flex flex-wrap justify-center">
+          <h2>出発地: {}</h2>
+          {
+            result.castles.map()
+          }
+        </div>
       </main>
       <Footer />
     </>
