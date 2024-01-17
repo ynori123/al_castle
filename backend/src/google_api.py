ccd7bf88f9e4a03b5e95b59786094ea0e2e8afe3
Se4weed <121135238+ynori123@users.noreply.github.com>
2024-01-17 15:55:41 +0900

    Update google_api.py

--- backend/src/google_api.py
+++ backend/src/google_api.py
@@ -1,7 +1,9 @@
 import requests
 from src.config import setting
+from src.model import CastleDistance
+# from logging import getLogger
 
-
+# logger = getLogger("uvicorn.app")
 def fetch_googlemap_api(origin: str, dest: str):
     
     url = "https://maps.googleapis.com/maps/api/distancematrix/json"
@@ -11,4 +13,14 @@ def fetch_googlemap_api(origin: str, dest: str):
         "key": setting.GOOGLE_MAP_API_KEY,
         "language": "ja",
     }
-    response = requests.get(url, params=params)
+    # logger.info(params)
+    req = requests.get(url, params=params)
+    req_json = req.json()
+    # logger.info(req_json)
+    res = CastleDistance(
+        castle_id_1=origin,
+        castle_id_2=dest,
+        distance=req_json.get("rows")[0].get("elements")[0].get("distance").get("value") / 1000,
+        time=req_json.get("rows")[0].get("elements")[0].get("duration").get("value")
+    )
+    return res
