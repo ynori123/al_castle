530c12c8d137ee104207aeae7e2da7901e956164
ynori123 <norisama1159@gmail.com>
2024-01-17 16:44:25 +0900

    レストランスキーマの追加

--- backend/src/schema.py
+++ backend/src/schema.py
@@ -4,6 +4,18 @@ from pydantic import (
     Field
 )
 
+
+class Restaurant(BaseModel):
+    name: str
+    prefecture: str
+    time: Optional[str] = Field(default="営業時間情報なし")
+    holiday: Optional[str] = Field(default="定休日情報なし")
+    genre = Optional[str] = Field(default="ジャンル情報なし")
+    url = Optional[str] = Field(default="URL情報なし")
+    
+    class config:
+        from_attributes = True
+
 class ResponseCastle(BaseModel):
     id: int
     name: str
@@ -11,12 +23,14 @@ class ResponseCastle(BaseModel):
     lat: float
     lng: float
     holiday: Optional[str] = Field(default="なし")
-    admission_time: Optional[str] = Field(default="No admission time information")
-    admission_fee: Optional[str] = Field(default="No admission fee information")
-    stamp: Optional[str] = Field(default="No stamp information")
+    admission_time: Optional[str] = Field(default="入場時間情報なし")
+    admission_fee: Optional[str] = Field(default="入場料金情報なし")
+    stamp: Optional[str] = Field(default="スタンプ情報なし")
+    restaurant: Optional[Restaurant] = Field(default="周辺のおすすめ飲食店情報なし")
 
     class Config:
         from_attributes = True
+
 
 class RequestCastle(BaseModel):
     castle_id: int
