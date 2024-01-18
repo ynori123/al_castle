03c31666c1b3ea201e7d07d76f6e43fd6c08f807
ynori123 <norisama1159@gmail.com>
2024-01-18 16:15:50 +0900

    redisの環境変数の実装

--- backend/src/config.py
+++ backend/src/config.py
@@ -7,5 +7,8 @@ import os
 class Setting(BaseSettings):
     DATABASE_URL: ClassVar[str] = os.environ.get('DATABASE_URL')
     GOOGLE_MAP_API_KEY: ClassVar[str] = os.environ.get('GOOGLE_MAP_API_KEY')
+    REDIS_HOST: ClassVar[str] = os.environ.get('REDIS_HOST')
+    REDIS_PORT: ClassVar[int] = os.environ.get('REDIS_PORT')
+    REDIS_PASSWORD: ClassVar[str] = os.environ.get('REDIS_PASSWORD')
 
 setting = Setting()
