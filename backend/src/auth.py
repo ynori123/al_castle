d97db8860a0b4a6dfde39d3f9f7ff18c57cf87c3
ynori123 <norisama1159@gmail.com>
2024-01-18 15:10:45 +0900

    認証の実装

--- backend/src/auth.py
+++ backend/src/auth.py
@@ -11,3 +11,12 @@ r = redis.Redis(
     password=setting.REDIS_PASSWORD,
     ssl=True
 )
+
+
+def set_token():
+    token = (str(uuid.uuid4())).encode()
+    now = datetime.now()
+    now = f"{now.year}-{now.month}-{now.day} {now.hour}:{now.minute}:{now.second}"
+    hashed_token = sha256(token).hexdigest()
+    r.set(hashed_token, now)
+    return token
