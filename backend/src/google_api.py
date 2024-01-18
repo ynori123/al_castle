9c8d396da3e8cd4c32a568a4cdba2324836c94dd
ynori123 <norisama1159@gmail.com>
2024-01-18 12:02:06 +0900

    住所検索の実装

--- backend/src/google_api.py
+++ backend/src/google_api.py
@@ -24,3 +24,30 @@ def fetch_googlemap_api(origin: str, dest: str):
         time=req_json.get("rows")[0].get("elements")[0].get("duration").get("value")
     )
     return res
+
+def search_castle_address(name: str):
+    url = "https://maps.googleapis.com/maps/api/geocode/json"
+    params = {
+        "address": name,
+        "language": "ja",
+        "components": {"country":"JP"},
+        "key": setting.GOOGLE_MAP_API_KEY,
+    }
+    req = requests.get(url, params=params)
+    req_json = req.json()
+    if req_json.get("status") != "OK":
+        params = {
+            "address": name+"跡",
+            "language": "ja",
+            "components": {"country":"JP"},
+            "key": setting.GOOGLE_MAP_API_KEY,
+        }
+        req = requests.get(url, params=params)
+        req_json = req.json()
+        if req_json.get("status") != "OK":
+            address = None
+        else:
+            address = req_json.get("results")[0].get("formatted_address")
+    else:
+        address = req_json.get("results")[0].get("formatted_address")
+    return address
