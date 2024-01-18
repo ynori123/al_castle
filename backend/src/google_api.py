4f4d4110884fcfc16b4abed5cf775dc1ce1e4d3f
ynori123 <norisama1159@gmail.com>
2024-01-18 12:56:50 +0900

    API呼び出し部分の実装

--- backend/src/google_api.py
+++ backend/src/google_api.py
@@ -1,11 +1,10 @@
 import requests
 from src.config import setting
-from src.model import CastleDistance
+from src.model import Castle, CastleDistance, Distance
 # from logging import getLogger
 
 # logger = getLogger("uvicorn.app")
-def fetch_googlemap_api(origin: str, dest: str):
-    
+def fetch_route_api(origin: str, dest: str):
     url = "https://maps.googleapis.com/maps/api/distancematrix/json"
     params = {
         "origins": origin,
@@ -17,14 +16,38 @@ def fetch_googlemap_api(origin: str, dest: str):
     req = requests.get(url, params=params)
     req_json = req.json()
     # logger.info(req_json)
-    res = CastleDistance(
-        castle_id_1=origin,
-        castle_id_2=dest,
+    res = Distance(
+        origin=origin,
+        dest=dest,
         distance=req_json.get("rows")[0].get("elements")[0].get("distance").get("value") / 1000,
         time=req_json.get("rows")[0].get("elements")[0].get("duration").get("value")
     )
     return res
 
+def fetch_route_api_during_castle(origin: Castle, dest: Castle):
+    url = "https://maps.googleapis.com/maps/api/distancematrix/json"
+    params = {
+        "origins": origin.address,
+        "destinations": dest.address,
+        "key": setting.GOOGLE_MAP_API_KEY,
+        "language": "ja",
+    }
+    # logger.info(params)
+    req = requests.get(url, params=params)
+    req_json = req.json()
+    res = Distance(
+        origin=origin.name,
+        dest=dest.name,
+        distance=req_json.get("rows")[0].get("elements")[0].get("distance").get("value") / 1000,
+        time=req_json.get("rows")[0].get("elements")[0].get("duration").get("value")
+    )
+    res_castle = CastleDistance(
+        castle_id_1=origin.id,
+        castle_id_2=dest.id,
+        distance=req_json.get("rows")[0].get("elements")[0].get("distance").get("value") / 1000,
+        time=req_json.get("rows")[0].get("elements")[0].get("duration").get("value")
+    )
+    return {"distance": res, "castle_distance": res_castle}
 def search_castle_address(name: str):
     url = "https://maps.googleapis.com/maps/api/geocode/json"
     params = {
